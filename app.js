8537dfd5997bcbb06f1e54e28f7e2d78a3530362
ginodnb <ginoarnone90@gmail.com>
2024-01-06 14:32:38 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+// let invert = document.createElement('div');
+// let balls;
+
+// let grid = document.createElement('section');
+// invert.classList.add('invert-pic');
+
+// const teams = ['ballbag', 'cunts', 'the blues', 'the shit ones']
+// teams.push("fuckers");
+// teams[1] = "TWATS";
+
+// console.log(teams)
+
+
+const quotes = ['Lifes a beach...', 'Clam Records, dont be a fish...', 'Strictly Shellerz', 'Hard times make hard clams, soft times make soft clams', 'I say Clam, you say Chowder', 'Why couldn\'t the clam make new friends?... It\'s not because she was too selfish, i think she just never opened up' ];
+// document.createElement('p');
+// quotes.classList.add('quote-para');
+// document.querySelector('.quote-para').innerHTML = `url(${quotes[i]})`;
+
+
+
+// quotes.sort(function() {
+//     return Math.random().innerHTML = quotes;
+// })
+
+// let result = ['']
+// for (i = 0; i < quotes.length; i++) {
+//     result.push(quotes[i]);
+
+// }
+
+// let returnResult = Math.floor()
+// let result = quotes.map(function(arr) {
+//     return arr;
+
+// })
+// console.log([result])
+
+const randomElement = quotes[Math.floor(Math.random() * quotes.length)];
+
+document.getElementById("quote-para").innerHTML = randomElement;
+
+console.log(randomElement)
+
+
+
+
+// let print = document.createElement('p');
+// print.classList.add('quote-p');
+// document.querySelector('.quote-p').innerHTML = result;
+
+
+
+
+
+
+const func1 = () => {
+    document.querySelector('.logo').style.display = "none";
+    document.querySelector('.invert-pic').style.display = "block";
+    document.querySelector('.main').style.backgroundColor = "black";
+    document.querySelector('.inner-main').style.justifyContent = "flex-start";
+    document.querySelector(".BUTTON_WXJ").style.display = "none"
+    document.querySelector(".navbar").style.backgroundColor = "black"
+    document.querySelector("#quote-para").style.display = "none"
+
+
+
+}
+
+
+const myFunction = () => {
+    let x = document.getElementById("myTopNav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar"
+    }
+};
+
+let clicker = document.querySelector("#tee");
+let backTee = document.querySelector("#back-tee")
+
+const doIt = () => {
+clicker.classList.toggle("other-tee")
+// clicker.classList.remove("shop-pics")
+// backTee.classList.toggle("shop-pics-back")
+// backTee.classList.toggle("tee")
+
+// backTee.classList.toggle("shop-pics")
+backTee.style = ("display: flex")
+}
+
+clicker.addEventListener("click", doIt)
+
+
+const reverse = () => {
+    clicker.classList.toggle("other-tee")
+    backTee.style = ("display: none")
+
+}
+
+backTee.addEventListener("click", reverse)
+
+
+
+// if (backTee.className = ("shop-pics")){
+//     backTee.classList.add("other-tee")
+//     console.log("shop-pics enabled")
+
+// } else if     (clicker.className = ("other-tee")){
+//     clicker.classList.add("back-tee")
+//     // clicker.classList.remove("other-tee")
+//     console.log("other tee is enabled")
+//     // clicker.classList.add("tee")
+// }
+
+
